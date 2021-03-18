d98243c7185b079bec38aca43dad299a2d8623fb
Listonoh <leestackcz@gmail.com>
2021-03-18 21:27:14 +0100

    moving library down a bit

--- web_django/Automata_lib/automata_test.py
+++
@@ -1,7 +0,0 @@
-from Automata_lib.automata import Automaton
-
-
-def test_1():
-    a = Automaton()
-    definition = a.definition
-    assert definition == a.definition
--- web_django/Automata_lib/tests/automata_test.py
+++
@@ -1,9 +0,0 @@
-# from automata import Automaton
-# import pytest
-from Automata_lib.automata import Automaton
-
-
-def test_11():
-    a = Automaton()
-    definition = a.definition
-    assert definition == a.definition
