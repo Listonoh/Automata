c7f4a4d2064cfb03a6f56d5f60f85544a5d945ae
Listonoh <leestackcz@gmail.com>
2021-03-21 13:20:17 +0100

    cosmetics + name changes

--- web_django/automata/views.py
+++ web_django/automata/views.py
@@ -1,7 +1,9 @@
 import json
+
 # import logging
 
 from Automata_lib import Automaton
+
 # from django.db import models
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
@@ -12,7 +14,7 @@ from .models import Automata
 
 def index(request):
     a = "something"
-    return render(request, 'automata_pages/result.html', {"automaton_result": a})
+    return render(request, "automata_pages/result.html", {"automaton_result": a})
 
 
 def computed_result(request):
@@ -20,57 +22,71 @@ def computed_result(request):
     defin = request.POST["automata"]
     a = Automaton()
     a.load(json.loads(defin))
-    correct = a.iterate_text(text)
+    correct = a.iterate_tape(text)
     return automata_form(request, False, True, True, json.dumps(a.definition), correct)
 
 
 def automata_form(
-        request, error=False, AutomatonParsed=False,
-        hasAutomaton=False, automat=None, result=None, word=""):
+    request,
+    error=False,
+    AutomatonParsed=False,
+    hasAutomaton=False,
+    automat=None,
+    result=None,
+    word="",
+):
     inputAutomata = InputAutomata()
     inputWord = InputWord()
-    if 'error' in request.GET:
+    if "error" in request.GET:
         error = True
-    return render(request, 'automata_pages/new_automata.html', {
-        'InputAutomata': inputAutomata,
-        'InputWord': inputWord,
-        'error': error,
-        'AutomatonParsed': AutomatonParsed,
-        'hasAutomaton': hasAutomaton,
-        'Automat': automat,
-        'result': result,
-        'word': word
-    })
+    return render(
+        request,
+        "automata_pages/new_automata.html",
+        {
+            "InputAutomata": inputAutomata,
+            "InputWord": inputWord,
+            "error": error,
+            "AutomatonParsed": AutomatonParsed,
+            "hasAutomaton": hasAutomaton,
+            "Automat": automat,
+            "result": result,
+            "word": word,
+        },
+    )
 
 
 def create_automat_from_form(request):
     # form = InputAutomata(request.POST, request.FILES)
     au = Automaton()
     if request.FILES:
-        a = request.FILES['file']
+        a = request.FILES["file"]
         b = json.load(a)
         au.load(b)
     else:
-        au.definition = json.loads(request.POST['text'])
+        au.definition = json.loads(request.POST["text"])
 
     return automata_form(request, False, True, True, json.dumps(au.definition))
 
 
 def saved_automatas(reqest):
-    automats = Automata.objects.order_by('-published_date')
-    context = {'list_of_automats': automats}
-    return render(reqest, 'automata_pages/saved_automata.html', context)
+    automats = Automata.objects.order_by("-published_date")
+    context = {"list_of_automats": automats}
+    return render(reqest, "automata_pages/saved_automata.html", context)
 
 
 def playground(reqest, automat_id):
     automat = get_object_or_404(Automata, pk=automat_id)
-    context = {"automat": automat, "log_level": InputInt(),
-               "automat_id": automat_id, "InputWord": InputWord()}
-    return render(reqest, 'automata_pages/playground.html', context)
+    context = {
+        "automat": automat,
+        "log_level": InputInt(),
+        "automat_id": automat_id,
+        "InputWord": InputWord(),
+    }
+    return render(reqest, "automata_pages/playground.html", context)
 
 
 def input(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         if "SubmitAutomaton" in request.POST:
             return create_automat_from_form(request)
         if "SubmitWord" in request.POST:
@@ -80,16 +96,16 @@ def input(request):
 
 
 def result(request, automat_id):
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
             automat = Automata.objects.get(pk=automat_id)
             text = request.POST["word"]
             log_level = int(request.POST["number"])
             a = Automaton(out_mode=log_level)
             a.load(automat.json_specification)
-            correct = a.iterate_text(text)
+            correct = a.iterate_tape(text)
             context = {"automaton_result": correct, "logs": a.logs.split("\n")}
-            return render(request, 'automata_pages/result.html', context)
+            return render(request, "automata_pages/result.html", context)
         except KeyError:
             raise Http404("Automat can not be loaded " + automat_id)
         # except:
